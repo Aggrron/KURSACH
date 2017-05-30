d9a6beb5b2827be5fab55f2269207961cb11fb3b
Aggrron <gsm10forhl2@yandex.ru>
2017-05-30 16:54:03 +0300

    Testing Changes 1

---
+++ Game2/Assets/Scripts/ChaseDeer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseDeer : MonoBehaviour {
+
+	public Transform EndPoint;
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.MoveTowards (transform.position, EndPoint.position, Time.deltaTime);
+	}
+}
--- Game2/Assets/Scripts/SawScript.cs
+++ Game2/Assets/Scripts/SawScript.cs
@@ -3,9 +3,11 @@ using UnityEngine.SceneManagement;
 
 public class SawScript : MonoBehaviour {
     
+	public  float rotateSpeed;
+
 	void Update ()
     {
-        transform.Rotate(new Vector3(0f, 0f, -20f));
+		transform.Rotate(new Vector3(0f, 0f, rotateSpeed));
     }
 
     void OnTriggerEnter2D(Collider2D other)
