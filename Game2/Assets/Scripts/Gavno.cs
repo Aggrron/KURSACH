7e3705903e55345e40f96aa68fa518e2bff7fbc8
Aggrron <gsm10forhl2@yandex.ru>
2017-06-04 18:11:17 +0300

    Дерьмо какое-то поделал

---
+++ Game2/Assets/Scripts/Gavno.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Gavno : MonoBehaviour {
+
+	// Use this for initialization
+	void OnMouseDown(Collider2D other) {
+		SceneManager.LoadScene("Training Scene");
+	}
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
