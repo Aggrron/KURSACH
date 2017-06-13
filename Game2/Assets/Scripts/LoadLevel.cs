9b6e5b978ef76dde1fef3ce2b8f8081e82af3c03
Aggrron <gsm10forhl2@yandex.ru>
2017-06-13 17:33:38 +0300

    Menu

---
+++ Game2/Assets/Scripts/LoadLevel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonoBehaviour {
+
+	public void startGame(){
+		SceneManager.LoadScene ("lvl 1"); 
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
